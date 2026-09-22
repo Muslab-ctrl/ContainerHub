ab0a1e90740f26dbd71c6069702c024d82aab897
Muslab <musiab.butt007@gmail.com>
2026-09-22 11:43:39 +0200

    Primi utilizzi di php.

---
+++ Primo_Progetto/www/public/hello-world.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <title>hello-world</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+                echo "Hello " .$_GET['nome'] . "!";
+            ?>
+        </h1>
+        <p>Questa è la mia prima prova con php.</p>
+    </body>
+</html>
